1589afecc91b199da148662586725333ef357a0f
Mac Dowell Prosper <macdowell@noukod.com>
2018-09-17 05:18:23 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-	if speed > 60 
+	if speed > 60
     true
   elsif speed < 40
     true
@@ -13,6 +13,3 @@ end
 def not_safe?(speed)
 	speed < 40 || speed > 60 ? true : false
 end
-	
-
-
